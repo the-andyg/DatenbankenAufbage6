f537c51f0f12360e2d0d2f22a4941b8393151869
Adr0312 <adrgra2001@gmail.com>
2022-12-19 20:18:16 +0100

    Batch funktioniert finally

--- src/main/java/org/example/DemoWeather.java
+++ src/main/java/org/example/DemoWeather.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.Date;
 import java.util.List;
+import java.util.Locale;
 
 import org.example.de.hska.iwii.db1.weather.JDBCWetter;
 import org.example.de.hska.iwii.db1.weather.model.Weather;
@@ -26,11 +27,12 @@ public class DemoWeather {
 		//    den Proxy-Parametern verwendet werden.
 		//    Proxy-Server: proxy.hs-karlsruhe.de
 		//    Port des Proxy-Servers: 8888
-		WeatherReader reader = new WeatherReader("proxy.hs-karlsruhe.de",8888, ConnectionData.name(), ConnectionData.password());
+		//WeatherReader reader = new WeatherReader("proxy.hs-karlsruhe.de",8888, ConnectionData.name(), ConnectionData.password());
+		WeatherReader reader = new WeatherReader();
 		//WeatherForecast weatherForecast = reader.readWeatherForecast(10519);
 		//List<Weather> weatherList = weatherForecast.getWeather();
 		//System.out.println(weatherList.get(0).getSunshine());
-		
+		System.out.println(reader.toString());
 		// 2. Auslesen von Informationen ueber einen oder mehrere Orte.
 		// Die Liste der Stationen ist hier verlinkt (4. Spalte enthaelt die ID):
 		// https://www.dwd.de/DE/leistungen/klimadatendeutschland/statliste/statlex_html.html
@@ -39,7 +41,7 @@ public class DemoWeather {
 		// 10321: Stuttgart-Degerloch
 		// Direktes Aufrufen der API:
 		// https://dwd.api.bund.dev/
-		WeatherForecast forecast = reader.readWeatherForecast(10519);
+		WeatherForecast forecast = reader.readWeatherForecast(10460);
 		if (forecast != null) {
 			for (Weather weather: forecast.getWeather()) {
 				System.out.println(weather.getDate() + ", " + weather.getMinTemp() / 10.0 + ", " + weather.getMaxTemp() / 10.0);
@@ -50,21 +52,21 @@ public class DemoWeather {
 		dbConnector.getConnection().setAutoCommit(false);
 		Statement statement = dbConnector.getConnection().createStatement();
 		String sql;
-		Statement prep = dbConnector.getConnection().createStatement();
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int[] stations = new int[]{10309,10442,12982,10552,10020};
 		for (int i = 0; i < 5; i++) {
-			String station = bufferedReader.readLine();
-			forecast = reader.readWeatherForecast(Integer.parseInt(station));
-			sql = "INSERT INTO wetter (stationsid, datum, mintemp, maxtemp, precipitation, sunshine) VALUES (%s,%s, %f,%f,%d,%d)";
+			forecast = reader.readWeatherForecast(stations[i]);
+			sql = "INSERT INTO wetter (stationsid, datum, mintemp, maxtemp, precipitation, sunshine) VALUES (%d,%s,%f,%f,%d,%d)";
 			for (Weather weather : forecast.getWeather()) {
-				prep = dbConnector.getConnection().prepareStatement(sql);
-				sql = String.format(sql, station, weather.getDate(), weather.getMinTemp(), weather.getMaxTemp(), weather.getPrecipitation(), weather.getSunshine());
-				prep.addBatch(sql);
+				String newsql = String.format(Locale.CANADA,sql, stations[i], "DATE '"+weather.getDate()+"'", weather.getMinTemp(), weather.getMaxTemp(), weather.getPrecipitation(), weather.getSunshine());
+				System.out.println(newsql);
+				statement.addBatch(newsql);
 			}
 		}
-
-		prep.executeBatch();
+		statement.executeBatch();
 		dbConnector.getConnection().commit();
+
+
 //		sql = "SELECT * FROM wetter WHERE stationsid = ?";
 //		prep = dbConnector.getConnection().prepareStatement(sql);
 //		System.out.println("stationsid: ");
